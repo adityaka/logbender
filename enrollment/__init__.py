411fa0685e068336aeef5e660374dc09be9f1179
adityaka <adityaka@live.com>
2024-10-23 15:28:27 +0530

    clean up the repo

--- enrollment/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# really funny way to set routes be carefull while doing this
-# there is a chance if the order is not right the app will keep crashing
-from enrollment import routes
